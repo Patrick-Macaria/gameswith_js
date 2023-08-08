4b5bb56aca456f3d0ef4dd352a1a2d112af04d7e
Patrick Macaria <patrickmacharianjeru@gmail.com>
2023-08-08 08:52:26 +0300

    Add files via upload
    A JS guessing game using loops and the browsers console  methods

---
+++ game_guessing.js
@@ -0,0 +1,22 @@
+let mnr;
+let cnr;
+let gnr;
+
+cnr = Math.floor(Math.random()*6);
+mnr = 6;
+gnr = Number(prompt("enter a number between 0 and 6"));
+
+
+
+
+if (gnr != cnr) {
+    prompt("Enter another number")
+    
+}else {alert("congratulations")}
+
+console.log(cnr);
+
+
+
+
+
